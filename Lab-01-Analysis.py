39af7ca9765084aa6800d2d960de7b47516e6bd6
Matthew Mehrtens <mcmehrtens@icloud.com>
2023-01-30 16:23:02 -0600

    Initial commit of files
    Stubbed out a couple of methods for Lab-01-Analysis.py.

---
+++ Lab-01-Analysis.py
@@ -0,0 +1,23 @@
+"""
+AER E 322 Lab 01
+Spring 2023
+Matthew Mehrtens, Peter Mikolitis, and Natsuki Oda
+
+This Python script imports our experimental data from the .csv file and then runs a number of post-
+processing algorithms on the data.
+"""
+
+
+# Starting point of the script
+def main():
+    print("Hello, world!")
+
+
+def import_data():
+    # TODO
+    return
+
+
+# Calls the main function
+if __name__ == "__main__":
+    main()
