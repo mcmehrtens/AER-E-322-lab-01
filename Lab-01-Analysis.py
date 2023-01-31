8e3a0dfc0fc73081fac2b8408c49636bb5cf9598
Matthew Mehrtens <mcmehrtens@icloud.com>
2023-01-30 20:14:58 -0600

    Added the import data function

--- Lab-01-Analysis.py
+++ Lab-01-Analysis.py
@@ -6,16 +6,22 @@ Matthew Mehrtens, Peter Mikolitis, and Natsuki Oda
 This Python script imports our experimental data from the .csv file and then runs a number of post-
 processing algorithms on the data.
 """
+import pandas as pd
+
+
+DATA_FILE = "Lab 01 Data.csv"
 
 
 # Starting point of the script
 def main():
-    print("Hello, world!")
+    # Import the data
+    data = import_data(DATA_FILE)
 
 
-def import_data():
-    # TODO
-    return
+# Imports the .csv file into a panda DataFrame
+def import_data(filename):
+    return pd.read_csv(filename, 
+                       skiprows=2)
 
 
 # Calls the main function
