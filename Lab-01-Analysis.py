0de4040df926568944a9e794b65e26323ea1dc0f
Matthew Mehrtens <mcmehrtens@icloud.com>
2023-01-31 10:58:14 -0600

    Added all the graphs and some preliminary smoothing.

--- Lab-01-Analysis.py
+++ Lab-01-Analysis.py
@@ -17,14 +17,73 @@ DATA_FILE = "Lab 01 Data.csv"
 def main():
     # Import the data
     data = import_data(DATA_FILE)
-    time = data["Time (s)"].to_numpy()
+    time = data["Time (s)"]
     displacement_time = time[0::20]
-    displacement = data["Displacement (mm)"].to_numpy()[0::20]
+    displacement = data["Displacement (mm)"][0::20]
     position = {
-        "run1": data["Position (m)"].to_numpy(),
-        "run2": data["Position (m).1"].to_numpy(),
-        "run3": data["Position (m).2"].to_numpy()
+        "run1": data["Position (m)"] * 1000,
+        "run2": data["Position (m).1"] * 1000,
+        "run3": data["Position (m).2"] * 1000
     }
+
+    # Graph Run 1 (Raw)
+    plt.figure()
+    plt.plot(time, position["run1"], label="Motion Sensor") 
+    plt.plot(displacement_time, displacement, label="Displacement Sensor")
+    plt.title("Run 1 (Raw)")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Displacement [mm]")
+    plt.legend(loc="best")
+    plt.grid()
+
+    # Graph Run 1 (Smoothed)
+    position["run1"] = position["run1"].rolling(window=5, center=True, closed="both").mean()
+
+    plt.figure()
+    plt.plot(time, position["run1"], label="Motion Sensor")
+    plt.title("Run 1 (Smoothed)")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Displacement [mm]")
+    plt.legend(loc="best")
+    plt.grid()
+
+    # Graph Run 2 (Raw)
+    plt.figure()
+    plt.plot(time, position["run2"], label="Motion Sensor")
+    plt.title("Run 2 (Raw)")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Displacement [mm]")
+    plt.legend(loc="best")
+    plt.grid()
+
+    # Graph Run 2 (Smoothed)
+    plt.figure()
+    plt.plot(time, position["run1"].rolling(window=5, center=True, closed="both").mean(), label="Motion Sensor")
+    plt.title("Run 2 (Smoothed)")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Displacement [mm]")
+    plt.legend(loc="best")
+    plt.grid()
+
+    # Graph Run 3 (Raw)
+    plt.figure()
+    plt.plot(time, position["run3"], label="Motion Sensor")
+    plt.title("Run 3 (Raw)")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Displacement [mm]")
+    plt.legend(loc="best")
+    plt.grid()
+
+    # Graph Run 3 (Smoothed)
+    plt.figure()
+    plt.plot(time, position["run1"].rolling(window=5, center=True, closed="both").mean(), label="Motion Sensor")
+    plt.title("Run 3 (Smoothed)")
+    plt.xlabel("Time [s]")
+    plt.ylabel("Displacement [mm]")
+    plt.legend(loc="best")
+    plt.grid()
+
+    plt.show()
 
 
 # Imports the .csv file into a panda DataFrame
